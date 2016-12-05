0b3cc5e5cbb7ff7a494fba177de2c1c9ffea72f5
Bastiaan Bakker <bbakker@xebia.com>
2016-12-05 22:27:48 +0100

    refactor out explicit exit(0)

--- d5/d5-1.go
+++ d5/d5-1.go
@@ -9,15 +9,12 @@ import (
 func main() {
 	i := 0
 	prefix := os.Args[1]
-	for index := 0; ; index++ {
+	for index := 0; i < 8; index++ {
 		data := fmt.Sprintf("%s%d", prefix, index)
 		sum := md5.Sum([]byte(data))
 		if sum[0] == 0 && sum[1] == 0 && (sum[2]&0xf0 == 0) {
 			fmt.Printf("%x %s\n", sum, data)
 			i++
-			if i == 8 {
-				os.Exit(0)
-			}
 		}
 	}
 }
