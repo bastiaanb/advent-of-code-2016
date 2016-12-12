3c6fe5dd52bd8421dfb58c5c71ccb7579ba3b0d6
Bastiaan Bakker <bbakker@xebia.com>
2016-12-12 22:48:39 +0100

    fix newline in output. remove comment. format lines

--- d12/d12-via-c.sh
+++ d12/d12-via-c.sh
@@ -7,25 +7,21 @@ OUT=/tmp/d12
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  long a=0;
-  long b=0;
-  long c=0;
-  long d=0;
+  long a=0, b=0, c=0, d=0;
 
 EOF
 
   pc=1
   while read instruction; do
-    echo "L$pc:"
-    echo "// $instruction"
+    echo -n "L$pc: "
     if [[ "$instruction" =~ ^cpy\ ([a-d]|[0-9\-]+)\ ([a-d]) ]]; then
-      echo "  ${BASH_REMATCH[2]}=${BASH_REMATCH[1]};"
+      echo "${BASH_REMATCH[2]}=${BASH_REMATCH[1]};"
     elif [[ "$instruction" =~ ^inc\ ([a-d]) ]]; then
-      echo "  ${BASH_REMATCH[1]}++;"
-    elif [[   "$instruction" =~ ^dec\ ([a-d]) ]]; then
-      echo "  ${BASH_REMATCH[1]}--;"
+      echo "${BASH_REMATCH[1]}++;"
+    elif [[ "$instruction" =~ ^dec\ ([a-d]) ]]; then
+      echo "${BASH_REMATCH[1]}--;"
     elif [[ "$instruction" =~ ^jnz\ ([a-d]|[0-9\-]+)\ ([0-9\-]+) ]]; then
-      echo "  if (${BASH_REMATCH[1]} != 0) goto L$((pc + ${BASH_REMATCH[2]}));"
+      echo "if (${BASH_REMATCH[1]} != 0) goto L$((pc + ${BASH_REMATCH[2]}));"
     else
       echo "not matched: $instruction"
       exit 1
@@ -35,7 +31,7 @@ EOF
 
   cat << EOF
 
-  printf("%ld %ld %ld %ld", a, b, c, d);
+  printf("%ld %ld %ld %ld\n", a, b, c, d);
 }
 EOF
 
