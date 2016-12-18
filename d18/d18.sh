645c64b74e577c6bf9291ad50414a8378614dee2
Bastiaan Bakker <bbakker@xebia.com>
2016-12-18 23:11:34 +0100

    add day 18

---
+++ d18/d18.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+line=$1
+times=${2:-10}
+
+count=0
+for((c=0; c < times; c++)); do
+  safe=${line//^}
+  ((count+=${#safe}))
+  printf "%2d: %s %d\n" $c $line $count
+  line=$(perl -pe 's/(?<=(.)).(?!\1)/X/g; tr/X^/^./' <<< ".$line.")
+  line=${line:1: -1}
+done
