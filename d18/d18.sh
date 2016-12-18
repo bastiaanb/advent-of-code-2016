c29d30150f45141570c89a53e3e9a7d0b80d2238
Bastiaan Bakker <bbakker@xebia.com>
2016-12-18 23:14:22 +0100

    move clipping of line into perl

--- d18/d18.sh
+++ d18/d18.sh
@@ -8,6 +8,5 @@ for((c=0; c < times; c++)); do
   safe=${line//^}
   ((count+=${#safe}))
   printf "%2d: %s %d\n" $c $line $count
-  line=$(perl -pe 's/(?<=(.)).(?!\1)/X/g; tr/X^/^./' <<< ".$line.")
-  line=${line:1: -1}
+  line=$(perl -pe 's/(?<=(.)).(?!\1)/X/g; tr/X^/^./; s/^.(.*).$/$1/' <<< ".$line.")
 done
