51a778a914a9704378c291341b86b2d08f45247a
Bastiaan Bakker <bbakker@xebia.com>
2016-12-27 22:56:06 +0100

    reformat d19-2

--- d19/d19-2.sh
+++ d19/d19-2.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 elfs=$(($1 - 1))
-pow=1
-while [[ $elfs -ge $pow ]]; do
-  ((pow*=3))
-done
+for((pow=1; elfs >= pow; pow*=3 )); do true; done
+((
+  pow/=3,
+  remainder=elfs%pow,
+  winner=remainder + ((elfs/pow)-1) * (pow + 1 + remainder)
+))
 
-((pow/=3))
-((remainder=elfs%pow))
-if [[ $elfs -ge $((pow+pow)) ]]; then
-  ((winner=pow+1+2*remainder))
-else
-  ((winner=remainder))
-fi
 echo $((winner+1))
