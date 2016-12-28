588b2910ef769db1aca93b1c579aa8f3d092a620
Bastiaan Bakker <bbakker@xebia.com>
2016-12-28 16:18:50 +0100

    cleanup d19-1b

--- d19/d19-1b.sh
+++ d19/d19-1b.sh
@@ -7,7 +7,7 @@ startelf=1
 offset=1
 
 while [[ $elfs -gt 1 ]]; do
-  echo "elfs: $elfs: startelf: $startelf offset: $offset"
+#  echo "elfs: $elfs: startelf: $startelf offset: $offset"
   if [[ $offset -eq 0 ]]; then
     ((startelf+=stepsize))
   fi
@@ -15,23 +15,6 @@ while [[ $elfs -gt 1 ]]; do
   ((elfs=(elfs+offset)/2))
   ((offset=(odd+offset)%2))
   ((stepsize+=stepsize))
-  echo "odd: $odd offset: $offset"
 done
 
-  if [[ $offset -eq 0 ]]; then
-    ((startelf+=stepsize))
-  fi
 echo "winner: $startelf"
-
-
-
-
-# 1
-# o 12345   o
-# 1 .x.x.   0  3
-# 0 x.x.x   1  2
-#
-# 0
-# o 1234    0
-# 1 .x.x    1  2
-# 0 x.x.    0  2
