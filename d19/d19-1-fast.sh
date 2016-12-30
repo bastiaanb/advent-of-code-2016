5678ad85b259716a026b9f20c8ed41fec93b8d3b
Bastiaan Bakker <bbakker@xebia.com>
2016-12-30 10:51:46 +0100

    cleanup d19-1-fast.sh

--- d19/d19-1-fast.sh
+++ d19/d19-1-fast.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-elfs=$1
+for((stepsize=1, startelf=1, offset=1, elfs=$1; elfs > 1; stepsize*=2)); do ((
+  startelf+=(1 - offset) * stepsize,
+  remainder=elfs % 2,
+  elfs=(elfs + offset) / 2,
+  offset=(remainder + offset) % 2
+)) done
 
-stepsize=1
-startelf=1
-offset=1
-
-while [[ $elfs -gt 1 ]]; do
-#  echo "elfs: $elfs: startelf: $startelf offset: $offset"
-  if [[ $offset -eq 0 ]]; then
-    ((startelf+=stepsize))
-  fi
-  ((odd=elfs%2))
-  ((elfs=(elfs+offset)/2))
-  ((offset=(odd+offset)%2))
-  ((stepsize+=stepsize))
-done
-
-echo "winner: $startelf"
+echo $startelf
