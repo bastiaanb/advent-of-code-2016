74434424d6afe0cb9b6eeca2750f4b70f9e56b93
Bastiaan Bakker <bbakker@xebia.com>
2017-01-04 23:30:07 +0100

    d11 haskell version

--- d11/d11.hs
+++ d11/d11.hs
@@ -2,9 +2,9 @@ moveOneFloor 1     = 1
 moveOneFloor 2     = 1
 moveOneFloor items = 2 + moveOneFloor(items - 1)
 
-moveMultiple c []     = 0
-moveMultiple c (x:xs) = moveOneFloor n + (moveMultiple n xs) where n = c + x
+moveMultipleFloors [x]    = moveOneFloor x
+moveMultipleFloors (x:xs) = moveOneFloor x + (moveMultipleFloors ((x + (head xs)):(tail xs)))
 
 main = do
-  print ( moveMultiple 0 [8,2,0] )
-  print ( moveMultiple 0 [8+4,2,0] )
+  print ( moveMultipleFloors [8,2,0] )
+  print ( moveMultipleFloors [8+4,2,0] )
