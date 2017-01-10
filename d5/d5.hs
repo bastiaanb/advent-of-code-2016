3fe97f17b5c7fe45dce68123a50586f9e4c2147b
Bastiaan Bakker <bbakker@xebia.com>
2017-01-10 12:19:49 +0100

    Day 5 part 1&2 in Haskell

--- d5/d5.hs
+++ d5/d5.hs
@@ -1,9 +1,25 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Crypto.Hash.MD5 as MD5
+import Data.Char
+import Data.List
+import Data.Maybe
 import Data.ByteString.Base16
 import qualified Data.ByteString.Char8 as C8
+import Crypto.Hash.MD5 as MD5
 
-fiveZerosHash = filter (C8.isPrefixOf "00000") $ map (encode.(MD5.hash).(C8.pack).("reyedfim" ++).show) [0..]
+fiveZerosHashes = map C8.unpack $ filter (C8.isPrefixOf $ C8.pack "00000") $ map (encode.MD5.hash.C8.pack.("reyedfim" ++).show) [0..]
 
-main = print $ map (\x -> C8.index x 5) $ take 8 $ fiveZerosHash
+findDigits state (x:xs) =
+  let i = digitToInt $ x !! 5
+  in
+    if i < length state && isNothing (state !! i) then
+      let newState = (take i state) ++ [Just $ x !! 6] ++ (drop (i + 1) state)
+        in
+          if all isJust newState then
+            map fromJust newState
+          else
+            findDigits newState xs
+    else
+      findDigits state xs
+
+main = do
+    print $ map (\x -> x !! 5) $ take 8 $ fiveZerosHashes
+    print $ findDigits (replicate 8 Nothing) fiveZerosHashes
